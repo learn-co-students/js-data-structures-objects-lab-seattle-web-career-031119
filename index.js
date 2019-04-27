279823371b7ff600a9b715752e0e77855e387069
github username <github email address>
2019-04-27 10:54:46 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+var driver = new Object()
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+  let newDriver = JSON.parse(JSON.stringify(driver))
+  newDriver[key] = value
+  return newDriver
+}
+
+function  destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  driver[key] = value
+  return driver
+}
+
+function deleteFromDriverByKey(driver, key) {
+  let newDriver = JSON.parse(JSON.stringify(driver))
+  delete newDriver[key]
+  return newDriver
+}
+
+function  destructivelyDeleteFromDriverByKey(driver, key, value) {
+  delete driver[key]
+  return driver
+}
